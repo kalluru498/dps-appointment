431b2f336e4d47fb52602e3704672f8ce4311f43
kalluru99 <kallurunaveen498@gmail.com>
2026-01-29 13:14:01 -0500

    updated testnotifier

--- tests/test_notifier.py
+++ tests/test_notifier.py
@@ -11,7 +11,7 @@ import os
 # Add src to path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 
-from utils.notifier import EmailNotifier
+from src.utils.notifier import EmailNotifier
 
 
 class TestEmailNotifier:
